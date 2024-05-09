a0ccaef4a24e3a515c5974388ee9cdb30c900397
juan <juanjose1092@gmail.com>
2024-05-09 13:39:44 -0500

    added new configs

---
+++ first.py
@@ -0,0 +1,45 @@
+import requests
+
+# Global variables
+access_token = ''
+refresh_token = ''
+client_id = '<client_id>'
+client_secret = '<client_secret>'
+
+def get_logs():
+    # The URL you're making the GET request to
+    url = ''
+
+    # The headers for the GET request
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    # Make the GET request
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 401:
+        refresh_access_token()
+
+
+def refresh_access_token():
+    url = ''
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'refresh_token',
+        'refresh_token': refresh_token
+    }
+    response = requests.post(url, data=data)
+    
+    if response.status_code == 200:
+        new_token_data = response.json()
+        global access_token
+        access_token = new_token_data['access_token']
+        global refresh_token
+        refresh_token = new_token_data['refresh_token']
+    else:
+        print('Failed to refresh access token')
